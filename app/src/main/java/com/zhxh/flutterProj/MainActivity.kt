12c8c3db3951c9b87da66f293d5710f308a48a73
zhxh <xiaohuizhu4@creditease.cn>
2022-09-27 18:01:20 +0800

    Initial commit

---
+++ app/src/main/java/com/zhxh/flutterProj/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.zhxh.flutterProj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+/// @time：2/8/22
+/// @author zhxh
+/// @description：
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
