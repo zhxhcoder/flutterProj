4cb5ef44ef0dd96d63bb1a211fd8a72d3f8682a5
zhxh <zhxhcoder@gmail.com>
2022-09-28 10:41:59 +0800

    资源文件与base库命名一样导致出错

--- base/src/main/java/com/zhxh/base/BaseMainActivity.java
+++ base/src/main/java/com/zhxh/base/BaseMainActivity.java
@@ -9,6 +9,6 @@ public class BaseMainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.base_main_layout);
     }
 }
